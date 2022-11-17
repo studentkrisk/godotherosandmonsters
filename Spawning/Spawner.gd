a6a37a3ca06941135d1f144d4c6130ac271be1bc
aayushrh <73722373+aayushrh@users.noreply.github.com>
2022-11-16 19:31:56 -0800

    Update Spawner.gd

--- Spawning/Spawner.gd
+++ Spawning/Spawner.gd
@@ -14,5 +14,4 @@ func _ready():
 		rng.randomize()
 		var y = global_position.y + rng.randf_range(-spawnRange, spawnRange)
 		object.global_position = Vector2(x, y)
-		print("Tree spawned at: " + str(object.global_position.x) + " ," + str(object.global_position.y))
 		add_child(object)
