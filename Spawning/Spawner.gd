fbe5e209c582dc47c35daf29fc6265abd283c7fa
aayushrh <73722373+aayushrh@users.noreply.github.com>
2022-11-16 19:14:42 -0800

    update

---
+++ Spawning/Spawner.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+export(int) var howMany = 0
+export(int) var spawnRange = 0;
+export var OBJECT : PackedScene
+
+onready var TREE = preload("res://Objects/Tree.tscn")
+
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	for i in range(howMany):
+		rng.randomize()
+		var object =  TREE.instance()
+		var x = global_position.x + rng.randf_range(-spawnRange, spawnRange)
+		rng.randomize()
+		var y = global_position.y + rng.randf_range(-spawnRange, spawnRange)
+		object.global_position = Vector2(x, y)
+		object.set_as_toplevel(true)
+		print("Tree spawned at: " + str(object.global_position.x) + " ," + str(object.global_position.y))
+		add_child(object)
